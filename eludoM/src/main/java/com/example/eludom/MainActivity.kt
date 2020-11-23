721bde5570433a60ee905cdd1633d3d4f4886a8b
robin <rodiontulub@gmail.com>
2020-11-23 14:45:07 +0200

    lab3 init commit

--- eludoM/src/main/java/com/example/eludom/MainActivity.kt
+++ eludoM/src/main/java/com/example/eludom/MainActivity.kt
@@ -5,22 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.eludom.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.lab1)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
     }
-
-    fun toastMe(viev: View){
-        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
-        myToast.show()
-    }
-
-    fun choose_food(viev: View){
-        val chooseIntent = Intent(this, MainActivity2::class.java)
-        startActivity(chooseIntent)
-    }
-
 
 }
